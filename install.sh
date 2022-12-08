dc545f01b11cc713ab84326f70504901a00bfb4e
L4rralde <larraldeortizemmanuelalejandro@gmail.com>
2022-12-08 08:43:48 -0600

    Adding echoes

--- install.sh
+++ install.sh
@@ -3,18 +3,22 @@ apt-get update
 apt-get curl
 apt-get install net-tools
 
+echo "Adding universe,multiverse,.."
 apt-add-repository universe
 apt-add-repository multiverse
 apt-add-repository restricted
 
+echo "sourcing ros keys"
 sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 
+echo "installing ros"
 apt-get update
 apt-get install ros-melodic-ros-desktop
 sh -c 'echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc'
 source ~/.bashrc
 
+echo "installing ros dependencies"
 apt install python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential
 apt install python-rosdep
 rosdep init
