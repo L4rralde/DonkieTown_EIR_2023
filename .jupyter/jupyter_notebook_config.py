c7d11710b85ad4ddf2fcda62ef9fe12a0e181140
L4rralde <larraldeortizemmanuelalejandro@gmail.com>
2023-01-05 17:54:47 -0600

    fixing notebook config after testing on base station

--- .jupyter/jupyter_notebook_config.py
+++ .jupyter/jupyter_notebook_config.py
@@ -3,9 +3,8 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 root_path = os.path.dirname(dir_path)
 
 c.NotebookApp.ip = '0.0.0.0'
-#c.NotebookApp.password = '' #Please, generate it from device
-#c.NotebookApp.notebook_dir = '{}/catkin_ws/src/autominy_msgs'.format(root_path)
-c.NotebookApp.default_url = '/lab?'.format(root_path)
+c.NotebookApp.password = 'argon2:$argon2id$v=19$m=10240,t=10,p=8$AGknHNZnibRf97E66hIxXQ$5TDgr9js64cMTG51iUDWPg' #Please, generate it from device
+c.NotebookApp.notebook_dir = '{}/'.format(dir_path)
 
 c.NotebookApp.port = 8888
 c.NotebookApp.open_browser = False
