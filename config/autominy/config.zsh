96f8be5c08ebd5bf19570e8b2d308bea536c9e56
L4rralde <larraldeortizemmanuelalejandro@gmail.com>
2023-02-03 14:27:20 -0600

    Applying previous change on config files for the other agents

--- config/autominy/config.zsh
+++ config/autominy/config.zsh
@@ -1,3 +1,5 @@
+cd $(dirname $(readlink -f "${BASH_SOURCE:-$0}"))
 export COURSE_PATH=$(git rev-parse --show-toplevel)
+cd - > /dev/null
 source $COURSE_PATH/catkin_ws/devel/setup.zsh
 #~/.local/bin/jupyter-lab --config=$COURSE_PATH/.jupyter/jupyter_notebook_config.py
--- config/client_config.bash
+++ config/client_config.bash
@@ -1,4 +1,6 @@
+cd $(dirname $(readlink -f "${BASH_SOURCE:-$0}"))
 export COURSE_PATH=$(git rev-parse --show-toplevel)
+cd - > /dev/null
 source $COURSE_PATH/catkin_ws/devel/setup.bash
 export ROS_MASTER_URI=192.168.100.$1
 export ROS_IP=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep 192.168.100.)
