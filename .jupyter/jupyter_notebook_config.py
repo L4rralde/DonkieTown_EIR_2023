9f47393fbafb1c2de62a20251c898e05863e23e3
L4rralde <larraldeortizemmanuelalejandro@gmail.com>
2023-02-03 14:00:50 -0600

    Adding course_path

--- .jupyter/jupyter_notebook_config.py
+++ .jupyter/jupyter_notebook_config.py
@@ -1,12 +1,12 @@
 import os 
+import sys
+root_path = os.getenv("COURSE_PATH")
+sys.path.append('{}/.jupyter'.format(root_path))
 import credentials
-
-dir_path = os.path.dirname(os.path.realpath(__file__))
-root_path = os.path.dirname(dir_path)
 
 c.NotebookApp.ip = '0.0.0.0'
 c.NotebookApp.password = credentials.pswd
-c.NotebookApp.notebook_dir = '{}/main.ipynb'.format(root_path)
+c.NotebookApp.notebook_dir = root_path
 
 c.NotebookApp.port = 8890
 c.NotebookApp.open_browser = False
