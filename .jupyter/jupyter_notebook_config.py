48d43cc009a0306addddd567ce194c6cbb881c48
L4rralde <larraldeortizemmanuelalejandro@gmail.com>
2023-02-03 12:45:01 -0600

    Isolating jupyter password

--- .jupyter/jupyter_notebook_config.py
+++ .jupyter/jupyter_notebook_config.py
@@ -1,9 +1,11 @@
 import os 
+import credentials
+
 dir_path = os.path.dirname(os.path.realpath(__file__))
 root_path = os.path.dirname(dir_path)
 
 c.NotebookApp.ip = '0.0.0.0'
-c.NotebookApp.password = 'argon2:$argon2id$v=19$m=10240,t=10,p=8$AGknHNZnibRf97E66hIxXQ$5TDgr9js64cMTG51iUDWPg' #Please, generate it from device
+c.NotebookApp.password = credentials.pswd
 c.NotebookApp.notebook_dir = '{}/main.ipynb'.format(root_path)
 
 c.NotebookApp.port = 8890
