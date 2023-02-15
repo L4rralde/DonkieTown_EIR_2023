ebd560939e4b4eccce31438dc2f9151d3804204a
L4rralde <larraldeortizemmanuelalejandro@gmail.com>
2023-02-15 13:23:10 -0600

    Adding libraries

--- .ubuntu_iso/install_libraries.sh
+++ .ubuntu_iso/install_libraries.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-sudo apt-get install python-pip
-pip install -U pygame --user 
+sudo apt install ros-melodic-tf #rf_ros
+sudo apt-get install ros-melodic-cv-bridge #cv_bridge
+sudo apt-get install ros-melodic-vision-opencv #cv2
+
+sudo apt-get install python-pip #pip
+pip install -U pygame #pygame
+pip install numpy #numpy
+python -m pip install scipy #scipy
+python -m pip install -U matplotlib #matplotlib
