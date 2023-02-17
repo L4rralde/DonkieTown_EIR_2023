263f866c84a25e3912755e6b19c11becfa51b9d7
L4rralde <larraldeortizemmanuelalejandro@gmail.com>
2023-02-17 08:01:37 -0600

    Skiping steps when installing as root

--- .ubuntu_iso/install_donkietown.sh
+++ .ubuntu_iso/install_donkietown.sh
@@ -1,8 +1,8 @@
 source /opt/ros/melodic/setup.sh
-cd /home/ubuntu/
+cd ~
 git clone https://github.com/L4rralde/DonkieTown_EIR_2023.git
-cd /home/ubuntu/DonkieTown_EIR_2023/catkin_ws
+cd ~/DonkieTown_EIR_2023/catkin_ws
 catkin_make
 cd -
-sudo chown -R 999.999 /home/ubuntu/DonkieTown_EIR_2023/
-sudo sh -c "echo 'source /home/ubuntu/DonkieTown_EIR_2023/catkin_ws/devel/setup.bash' >> /home/ubuntu/.bashrc"
+#sudo chown -R 999.999 /home/ubuntu/DonkieTown_EIR_2023/
+sudo sh -c "echo 'source ~/DonkieTown_EIR_2023/catkin_ws/devel/setup.bash' >> ~/.bashrc"
