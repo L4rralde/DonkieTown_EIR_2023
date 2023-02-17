a31300cf82e961a637d8192ba8a51ddb79be8c0e
L4rralde <larraldeortizemmanuelalejandro@gmail.com>
2023-02-16 20:29:54 -0600

    Adding more comments

--- catkin_ws/src/hola_ros/src/publicador.cpp
+++ catkin_ws/src/hola_ros/src/publicador.cpp
@@ -1,36 +1,36 @@
-// This program publishes randomly−generated velocity messages for turtlesim
+// Este programa publica mensajes de velocidad aleatoriamente generados para turtlesim
 #include<ros/ros.h>
-#include<geometry_msgs/Twist.h>	// For geometry_msgs::Twist
-#include<stdlib.h>			// For rand()andRAND_MAX
+#include<geometry_msgs/Twist.h>	//Para geometry_msgs::Twist
+#include<stdlib.h>			//Para rand() y RAND_MAX
 
 int main(int argc, char **argv){
-// Initialize the ROS system and become a node
+// Inicializa el sistema de ROS.
 ros::init(argc, argv, "publish_velocity");
 ros::NodeHandle nh;
 
-// Create a publisher object
+// Crea un objeto tipo publisher
 ros::Publisher pub = nh.advertise<geometry_msgs::Twist>("turtle1/cmd_vel", 1000);
 
-// Seed the random number generator
+// Agrega una semilla a la generación de números aleatorios.
 srand(time(0));
 
-// Loop at 2Hz until the node is shut down
+//Repite a 2Hz hasta que el nodo se apague
 ros::Rate rate(2);
 
 while(ros::ok()){
-	// Create and fill in the message. The other four
-	// fields, which are ignored byturtlesim, default to 0
+	// Crea y llena el mensaje de tipo velocidad. 
+	// Por defecto todos los campos valen 0 al construir el objeto.
 	geometry_msgs::Twist msg;
 	msg.linear.x = double(rand())/double(RAND_MAX);
 	msg.angular.z = 2*double(rand())/double(RAND_MAX) - 1;
-		// Publish the message
+		// Publica el mensaje:
 	pub.publish(msg);
 
-	// Send a message to ros out with the details
+	// Envía un mensaje a rosout con los detalles
 	ROS_INFO_STREAM("Sending random velocity command:"
 		<< " linear="<<msg.linear.x << " angular=" << msg.angular.z);
 
-	// Wait until it's time for another iteration
+	// Espera hasta que sea el tiempo para la próxima iteración.
 	rate.sleep();
 	}
 }
--- catkin_ws/src/hola_ros/src/subscriptor.cpp
+++ catkin_ws/src/hola_ros/src/subscriptor.cpp
@@ -1,6 +1,7 @@
 #include<ros/ros.h>
 #include<geometry_msgs/Twist.h>
 
+// Funcion callback que es llamada siempre que se recibe un mensaje en el tópic al que se subscribe
 void callback(const geometry_msgs::Twist& msg){
 	ROS_INFO("Recibi componente lineal [%f, %f, %f]", msg.linear.x, msg.linear.y, msg.linear.z);
 	ROS_INFO("Recibi componente angular [%f, %f, %f]", msg.angular.x, msg.angular.y, msg.angular.z);
@@ -9,7 +10,7 @@ void callback(const geometry_msgs::Twist& msg){
 int main(int argc, char **argv){
 	ros::init(argc, argv, "subscibre_velocity");
 	ros::NodeHandle nh;
-
+	// Objeto de subscriptor al tópico /turtle1/cmd_vel
 	ros::Subscriber sub = nh.subscribe("/turtle1/cmd_vel",1000,callback);
 
 	ros::spin();
